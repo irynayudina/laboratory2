0beef502c4364b792dfe49841877eadaac4d3f2b
Iryna Yudina <yudinaira4444@gmail.com>
2020-10-05 20:54:33 +0300

    3 left

--- laboratory2.cpp
+++ laboratory2.cpp
@@ -1,163 +1,4 @@
-﻿//#include <bits/stdc++.h> 
-#include <iostream>
-using namespace std;
-
-// Custom class to handle Linked List operations 
-// Operations like push_back, push_front, pop_back, 
-// pop_front, erase, size can be added here 
-template <typename T>
-class LinkedList
-{
-    // Forward declaration 
-    class Node;
-
-public:
-    LinkedList<T>() noexcept
-    {
-        // caution: static members can't be 
-        // initialized by initializer list 
-        m_spRoot = nullptr;
-    }
-
-    // Forward declaration must be done 
-    // in the same access scope 
-    class Iterator;
-
-    // Root of LinkedList wrapped in Iterator type 
-    Iterator begin()
-    {
-        return Iterator(m_spRoot);
-    }
-
-    // End of LInkedList wrapped in Iterator type 
-    Iterator end()
-    {
-        return Iterator(nullptr);
-    }
-
-    // Adds data to the end of list 
-    void push_back(T data);
-
-    void Traverse();
-
-    // Iterator class can be used to 
-    // sequentially access nodes of linked list 
-    class Iterator
-    {
-    public:
-        Iterator() noexcept :
-            m_pCurrentNode(m_spRoot) { }
-
-        Iterator(const Node* pNode) noexcept :
-            m_pCurrentNode(pNode) { }
-
-        Iterator& operator=(Node* pNode)
-        {
-            this->m_pCurrentNode = pNode;
-            return *this;
-        }
-
-        // Prefix ++ overload 
-        Iterator& operator++()
-        {
-            if (m_pCurrentNode)
-                m_pCurrentNode = m_pCurrentNode->pNext;
-            return *this;
-        }
-
-        // Postfix ++ overload 
-        Iterator operator++(int)
-        {
-            Iterator iterator = *this;
-            ++* this;
-            return iterator;
-        }
-
-        bool operator!=(const Iterator& iterator)
-        {
-            return m_pCurrentNode != iterator.m_pCurrentNode;
-        }
-
-        int operator*()
-        {
-            return m_pCurrentNode->data;
-        }
-
-    private:
-        const Node* m_pCurrentNode;
-    };
-
-private:
-
-    class Node
-    {
-        T data;
-        Node* pNext;
-
-        // LinkedList class methods need 
-        // to access Node information 
-        friend class LinkedList;
-    };
-
-    // Create a new Node 
-    Node* GetNode(T data)
-    {
-        Node* pNewNode = new Node;
-        pNewNode->data = data;
-        pNewNode->pNext = nullptr;
-
-        return pNewNode;
-    }
-
-    // Return by reference so that it can be used in 
-    // left hand side of the assignment expression 
-    Node*& GetRootNode()
-    {
-        return m_spRoot;
-    }
-
-    static Node* m_spRoot;
-};
-
-template <typename T>
-/*static*/ typename LinkedList<T>::Node* LinkedList<T>::m_spRoot = nullptr;
-
-template <typename T>
-void LinkedList<T>::push_back(T data)
-{
-    Node* pTemp = GetNode(data);
-    if (!GetRootNode())
-    {
-        GetRootNode() = pTemp;
-    }
-    else
-    {
-        Node* pCrawler = GetRootNode();
-        while (pCrawler->pNext)
-        {
-            pCrawler = pCrawler->pNext;
-        }
-
-        pCrawler->pNext = pTemp;
-    }
-}
-
-template <typename T>
-void LinkedList<T>::Traverse()
-{
-    Node* pCrawler = GetRootNode();
-
-    while (pCrawler)
-    {
-        cout << pCrawler->data << " ";
-        pCrawler = pCrawler->pNext;
-    }
-
-    cout << endl;
-}
-
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 struct product {
     string name;
@@ -199,15 +40,32 @@ public:
     {
         node* temp = new node;
         temp = head;
-        do 
+        if (temp != NULL) {
+        do
         {
             cout << temp->data.name << "\n";
             temp = temp->next;
         } while (temp != head);
+        }
     }
-    void moove_by_n_possitions() 
+    node* get_head() {
+        return head;
+    }
+    int amount() {
+        int Amount = 0;
+        node* temp = new node;
+        temp = head;
+        do
+        {
+            temp = temp->next;
+            ++Amount;
+        } while (temp != head);
+        return Amount;
+    }
+    //required methods
+    void moove_by_n_possitions(node * temp) 
     {
-
+        
     }
     CircularList copy()
     {
@@ -243,7 +101,6 @@ public:
     {
         node* previous = new node;
         node* current = new node;
-       // previous = head;
         current = head;
         while (n > 1) 
         {
@@ -252,6 +109,7 @@ public:
             --n;
         }
         previous->next = current->next;
+        delete current;
     }
     void insert_after_position(int pos, product value)
     {
@@ -268,87 +126,83 @@ public:
         pre->next = temp;
         temp->next = cur;
     }
-    void clean() 
-    {
+    CircularList list_of_same(CircularList object) {
+        CircularList result;
+        node* point = new node;
+        point = head;
+        node* point_obj = new node;
+        node* obj_head = object.get_head();
+        do
+        {
+            do 
+            {
+                point_obj = obj_head;
+                if (point == point_obj) 
+                {
+                    result.createnode(point->data);
+                }
+                point_obj = point_obj->next;
+            
+            } while (point_obj != obj_head);            
+            point = point->next;            
+        } while (point != head);
+        return object;
+    }
+    void sort_by_increase_decrease() {
 
     }
-    /*
-    void delete_first()
-    {
+    void delete_each_n(int n) {
+        int count = 1;
+        int amount = 0;
         node* temp = new node;
         temp = head;
-        head =  head->next;
-        delete temp;
+        do
+        {
+            temp = temp->next;
+            ++amount;
+        } while (temp != head);
+        for (int i = 0; i < amount; i++) {
+            if (count % n == 0) {
+                delete_from_n_possition(count);
+            }
+            ++count;
+        }
+        
     }
-    void delete_last()
+    void clean() 
     {
-        node* current = new node;
+        /*node* temp = head;
+        node* current = head;
+        head = head->next;
+        delete head;
+        do
+        {
+            temp = temp->next;
+        } while (temp != head);*/
+        node* temp = new node;
+        node* temp_head = new node;
         node* previous = new node;
+        temp = head;
+        temp_head = head;
+            do
+            {
+                previous = temp;
+                temp = temp->next;
+                delete previous;
+            } while (temp != temp_head);
+       /* int n = amount();
+        node* previous = new node;
+        node* current = new node;
         current = head;
-        while (current->next != NULL)
+        while (n-1)
         {
             previous = current;
             current = current->next;
-        }
-        tail = previous;
-        previous->next = NULL;
-        delete current;
-    }*/
-    class Iterator
-    {
-    public:
-        Iterator() noexcept :
-            m_pCurrentNode(m_spRoot) { }
-
-        Iterator(const Node* pNode) noexcept :
-            m_pCurrentNode(pNode) { }
-
-        Iterator& operator=(Node* pNode)
-        {
-            this->m_pCurrentNode = pNode;
-            return *this;
-        }
-
-        // Prefix ++ overload 
-        Iterator& operator++()
-        {
-            if (m_pCurrentNode)
-                m_pCurrentNode = m_pCurrentNode->pNext;
-            return *this;
-        }
-
-        // Postfix ++ overload 
-        Iterator operator++(int)
-        {
-            Iterator iterator = *this;
-            ++* this;
-            return iterator;
-        }
-
-        bool operator!=(const Iterator& iterator)
-        {
-            return m_pCurrentNode != iterator.m_pCurrentNode;
-        }
-
-        int operator*()
-        {
-            return m_pCurrentNode->data;
-        }
-
-    private:
-        const node* m_pCurrentNode;
-    };
-    void delete_position(int pos)
-    {
-        node* current = new node;
-        node* previous = new node;
-        current = head;
-        for (int i = 1; i < pos; i++)
-        {
-            previous = current;
-            current = current->next;
-        }
-        previous->next = current->next;
+            previous->next = current->next;
+            delete current;
+            --n;
+        }*/
+        
     }
 };
 
@@ -356,9 +210,17 @@ public:
 int main()
 {
     cout << "Hello World!\n";
-    CircularList A, B, C;
+    CircularList A, B, C, E;
     product p1;
     p1.name = "first";
+    A.createnode(p1);
+    A.createnode(p1);
+    A.createnode(p1);
+    A.createnode(p1);
+    A.createnode(p1);
+    A.createnode(p1);
+    A.createnode(p1);
+    A.createnode(p1);
     A.createnode(p1);
     A.createnode(p1);
     A.createnode(p1);
@@ -369,30 +231,12 @@ int main()
     B.display();
     C = A.merge(B);
    // C.display();
-    A.delete_from_n_possition(1);
+    A.delete_each_n(2);
     //C.delete_position(1);
-    cout << "after cutting";
+    cout << "after cutting"<<endl;
     A.display();
-
-
-    LinkedList<int> list;
-
-    // Add few items to the end of LinkedList 
-    list.push_back(1);
-    list.push_back(2);
-    list.push_back(3);
-
-    cout << "Traversing LinkedList through method" << endl;
-    list.Traverse();
-
-    cout << "Traversing LinkedList through Iterator" << endl;
-    for (LinkedList<int>::Iterator iterator = list.begin();
-        iterator != list.end(); iterator++)
-    {
-        cout << *iterator << " ";
-    }
-
-    cout << endl;
-
-    return 0;
+    cout << "after cleaning"<<endl;
+    A.clean();
+   // A.display();
+   //cout <<  A.amount();
 }
